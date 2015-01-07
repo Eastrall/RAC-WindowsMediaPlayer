7157b138063f9460e228162598c151d9e767fc19
Filipe GOMES PEIXOTO <gomespeixoto.filipe@gmail.com>
2015-01-07 16:39:39 +0100

    Add base video layout

---
+++ WindowsMediaPlayer/Pages/Video/Home/HomeVideoViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*--------------------------------------------------------
+ * HomeVideoViewModel.cs - file description
+ * 
+ * Version: 1.0
+ * Author: Filipe
+ * Created: 07/01/2015 16:38:56
+ * 
+ * Notes:
+ * -------------------------------------------------------*/
+
+namespace WindowsMediaPlayer.Pages.Video.Home
+{
+    public class HomeVideoViewModel
+    {
+        #region FIELDS
+        #endregion
+
+        #region PROPERTIES
+        #endregion
+
+        #region CONSTRUCTORS
+
+        public HomeVideoViewModel()
+        {
+        }
+
+        #endregion
+
+        #region METHODS
+        #endregion
+    }
+}
